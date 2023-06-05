25d07bcf3addc3af7047ac5d15acf07d030f62cf
Kisang Hwang <hgd4859@gmail.com>
2023-06-05 14:25:13 -0700

    build33

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
 import DoorSlidingOutlinedIcon from '@mui/icons-material/DoorSlidingOutlined';
 
 export default function App() {
-  // const [selectedPark, setSelectedPark] = useState(null);
+  const [selectedPark, setSelectedPark] = useState(null);
   // eslint-disable-next-line 
   const [lat, setLat] = useState([]);
   // eslint-disable-next-line 
@@ -85,12 +85,13 @@ export default function App() {
           }
         });
       }
+      setSelectedPark(getData);
       setToggle(true);
       
     }
     // if(message)
   })
-
+  alert(selectedPark.geometry.coordinates[0])
   return (
     <>
       <MapGL
